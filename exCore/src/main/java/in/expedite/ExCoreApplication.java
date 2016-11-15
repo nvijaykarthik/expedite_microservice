8fc65ea02d127667158f204ba8ee6af13e51430f
vijaykarthik <nvijaykarthik@gmail.com>
2016-11-15 16:10:55 +0530

    Initial Commit

---
+++ exCore/src/main/java/in/expedite/ExCoreApplication.java
@@ -0,0 +1,12 @@
+package in.expedite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExCoreApplication.class, args);
+	}
+}
